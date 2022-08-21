7fef63687dfcf87887e60adc09c3fa2934ecb05e
Sunit Corneleous <sunit15-10591@diu.edu.bd>
2022-08-21 23:53:05 +0600

    adding player to list

--- js/addPlayers.js
+++ js/addPlayers.js
@@ -1,8 +1,29 @@
 const allSelectButtons = document.querySelectorAll(".select-btn");
 
+function setPlayerInList(player) {
+  const orderedList = document.getElementById("selected-players");
+
+  const li = document.createElement("li");
+  li.innerText = player;
+  orderedList.appendChild(li);
+}
+
+function disableButton(e) {
+  const button = e.target;
+  button.setAttribute("disabled", "true");
+  button.classList.add("btn-disable");
+}
+
 for (const button of allSelectButtons) {
   button.addEventListener("click", function (event) {
     const playerName = event.target.parentNode.childNodes[1].innerText;
-    console.log(playerName);
+    const orderedList = document.querySelectorAll("ol li");
+
+    if (orderedList.length < 5) {
+      setPlayerInList(playerName);
+      disableButton(event);
+    } else {
+      alert("Maximum players added");
+    }
   });
 }
